301545bb3df7df701d12aa36ec52b490c5f1dbcb
Daniel Bast <2790401+dbast@users.noreply.github.com>
2024-10-17 11:11:28 +0200

    Debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,8 @@ if [ "$(uname -m)" = "ppc64le" ]; then
     export LDFLAGS="$LDFLAGS -mcmodel=large"
 fi
 
+env | sort
+
 cd $SRC_DIR
 go build -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -tags extended -trimpath -v -o $PREFIX/bin/hugo
 go-licenses save . --save_path ./library_licenses
