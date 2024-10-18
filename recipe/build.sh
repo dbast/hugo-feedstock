8d13ea42ed5ba391792fe70f9bc789d261847196
Daniel Bast <2790401+dbast@users.noreply.github.com>
2024-10-18 17:38:38 +0200

    print commands

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,6 +16,6 @@ fi
 env | sort
 
 cd $SRC_DIR
-go build -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -tags extended -trimpath -v -o $PREFIX/bin/hugo
+go build -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -tags extended -trimpath -v -x -o $PREFIX/bin/hugo
 go-licenses save . --save_path ./library_licenses
 chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
