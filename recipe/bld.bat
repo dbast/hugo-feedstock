a8033c2af58607324b57ca4d35e0fb8d03fdea10
Daniel Bast <2790401+dbast@users.noreply.github.com>
2026-05-30 16:33:52 +0200

    Add Chroma license workaround

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,4 +5,8 @@ set CGO_ENABLED=1
 
 cd %SRC_DIR%
 go build -buildmode=exe -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge -extldflags '-static'" -tags extended -trimpath -v -o %LIBRARY_PREFIX%\bin\hugo.exe || exit 1
-go-licenses save . --save_path .\library_licenses || exit 1
+rem Chroma's COPYING contains MIT plus OFL text, which go-licenses reports as unknown.
+go-licenses save . --save_path .\library_licenses --ignore github.com/alecthomas/chroma/v2 || exit 1
+mkdir .\library_licenses\github.com\alecthomas\chroma\v2 || exit 1
+for /f "usebackq delims=" %%i in (`go list -m -f "{{ .Dir }}" github.com/alecthomas/chroma/v2`) do set CHROMA_DIR=%%i
+copy "%CHROMA_DIR%\COPYING" .\library_licenses\github.com\alecthomas\chroma\v2\COPYING || exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,8 @@ set -ex
 
 cd $SRC_DIR
 go build -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -tags extended -trimpath -v -o $PREFIX/bin/hugo
-go-licenses save . --save_path ./library_licenses
+# Chroma's COPYING contains MIT plus OFL text, which go-licenses reports as unknown.
+go-licenses save . --save_path ./library_licenses --ignore github.com/alecthomas/chroma/v2
+mkdir -p ./library_licenses/github.com/alecthomas/chroma/v2
+cp "$(go list -m -f '{{ .Dir }}' github.com/alecthomas/chroma/v2)/COPYING" ./library_licenses/github.com/alecthomas/chroma/v2/COPYING
 chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
