f64483015ed97068640d3c3814027dad7880cadd
Daniel Bast <2790401+dbast@users.noreply.github.com>
2026-07-16 15:34:29 +0200

    Use {{ compiler('go-nocgo') }} on win-arm64
    Also cleanup things as activation scripts already set variables.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,15 +5,8 @@ set GO111MODULE=on
 cd %SRC_DIR%
 
 if /I "%HOST_PLATFORM%"=="win-arm64" (
-    rem conda-forge has no gcc/go-cgo cross-compiler for win-arm64, so cgo
-    rem (needed by -tags extended) isn't available here. Build the plain
-    rem non-extended binary instead.
-    set CGO_ENABLED=0
-    set GOOS=windows
-    set GOARCH=arm64
     go build -buildmode=exe -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -trimpath -v -o %LIBRARY_PREFIX%\bin\hugo.exe || exit 1
 ) else (
-    set CGO_ENABLED=1
     go build -buildmode=exe -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge -extldflags '-static'" -tags extended -trimpath -v -o %LIBRARY_PREFIX%\bin\hugo.exe || exit 1
 )
 rem Chroma's COPYING contains MIT plus OFL text, which go-licenses reports as unknown.
